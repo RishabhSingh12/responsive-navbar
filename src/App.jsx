b29b881c7c5758bce96034f3cceb9bc43f98a9a5
RISHABH KUMAR <rishabh.kumar@gida.io>
2022-12-22 19:35:48 +0530

    Modified structure, stylings and logic for the responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [toggle, setToggle] = useState(false);
+  const toggleHandler =(e) => {
+    setToggle(!toggle);
+    // e.target.style.classList.toggle("close-menu");
+  }
 
-  return (
+  return (<>
     <div className="navbar-container">
       <nav className='navbar'>
         <div className='nav-title'>User's name</div>
@@ -15,11 +18,19 @@ function App() {
           <p className="option-items">Project</p>
           <p className="option-items">Contact</p>
         </div>
-        <div className="option-small-container">
-          <i className="fa-solid fa-bars"></i>
+        <div className="option-small-container" onClick={toggleHandler}>
+          <button className={`menu ${toggle ? "bar-menu" : "close-menu"}`}>
+            <p></p>
+            <p></p>
+            <p></p>
+          </button>
         </div>
       </nav>
     </div>
+    {toggle && <div className="toggle-menu" style={{marginTop:"100px"}}>
+      sign in
+    </div>}
+  </>
   )
 }
 
